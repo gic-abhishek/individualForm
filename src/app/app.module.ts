536ca6c9755a7cbe569d053aedab6b88ae272855
gic-abhishek <94887657+gic-abhishek@users.noreply.github.com>
2022-07-11 20:00:23 +0530

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { LayoutModule } from "@progress/kendo-angular-layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { IconsModule } from "@progress/kendo-angular-icons";
+import { HttpClientModule } from '@angular/common/http';
+import { ButtonsModule } from "@progress/kendo-angular-buttons";
+import { InputsModule } from "@progress/kendo-angular-inputs";
+import { LabelModule } from "@progress/kendo-angular-label";
+import { DropDownsModule } from "@progress/kendo-angular-dropdowns";
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DialogModule } from '@progress/kendo-angular-dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    IconsModule,
+    ButtonsModule,
+    InputsModule,
+    LabelModule,
+    DropDownsModule,
+    BrowserAnimationsModule,
+    DateInputsModule,
+    HttpClientModule,
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,82 @@
+import { formatDate } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor(private http:HttpClient) { }
+  allData:any
+  public opened = false;
+  inputDataObj:any;
+  title:any;
+  apiUrl='http://10.2.60.130:8487/api/india/onlineapi/'
+
+  searchForm:FormGroup=new FormGroup({
+    name:new FormControl('',Validators.required),
+  din:new FormControl(),
+  dob:new FormControl('',Validators.required),
+  contexts:new FormControl(),
+  address:new FormControl(),
+  father_name:new FormControl(),
+  state: new FormControl(),
+  startdate:new FormControl(new Date())
+  })
+  ngOnInit(): void {
+  }
+
+
+  getValueLoop(item:any){
+   return item
+  }
+
+  checkValueObj(obj:any){
+    
+    if(typeof(obj) === 'string' && obj.includes('{')){
+      obj = JSON.parse(obj)
+      return obj
+    }
+    else{
+      return obj
+    }
+  }
+
+
+
+  public close(status: string): void {
+    this.opened = false;
+  }
+
+   openDialog(data:any,title:any) {
+this.title=title
+    this.inputDataObj = JSON.parse(data)
+    this.opened = true;
+  }
+
+  onSearch(){
+    let obj={
+      name:this.searchForm.value.name,
+      din:this.searchForm.value.din,
+      dob:formatDate(this.searchForm.value.startdate, 'dd.MM.yyyy', 'en'),
+      contexts:this.searchForm.value.contexts,
+      address:this.searchForm.value.address,
+      father_name:this.searchForm.value.father_name,
+      state: this.searchForm.value.state,
+      startdate:formatDate(this.searchForm.value.startdate, 'dd.MM.yyyy', 'en'),
+                
+    }
+  this.http.post(this.apiUrl,obj).subscribe(el=>{
+    console.log(el)
+    this.allData=el
+      console.log(this.allData)   
+      this.searchForm.reset();
+  })
+}
+
+
+}
