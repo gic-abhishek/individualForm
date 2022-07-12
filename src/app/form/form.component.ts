b03bd47301727e63ec75e3cc829677a51a21e262
gic-abhishek <94887657+gic-abhishek@users.noreply.github.com>
2022-07-12 17:37:36 +0530

    update

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,8 @@ import { formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-form',
@@ -9,10 +11,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  message:any;
 
   constructor(private http:HttpClient) { }
-  allData:any
+  allData:any=[]
+  public min: Date = new Date("2020.01.01 13:02");
+
+  public max: Date = new Date("2020.01.01 12:01");
   public opened = false;
+  openedDialog:boolean=false;
   inputDataObj:any;
   title:any;
   apiUrl='http://10.2.60.130:8487/api/india/onlineapi/'
@@ -26,7 +33,7 @@ export class FormComponent implements OnInit {
   address:new FormControl(),
   father_name:new FormControl(),
   state: new FormControl(),
-  startdate:new FormControl(new Date())
+  startdate:new FormControl()
   })
   ngOnInit(): void {
   }
@@ -54,10 +61,23 @@ export class FormComponent implements OnInit {
     }
   }
 
+  age18Check(birthday:any) {
+    console.log(birthday)
+    let data=  moment(birthday).add(18, 'years') <= moment();
+    if(data === false){
+      this.openedDialog=true;
+      this.searchForm.patchValue({
+        dob:''
+      })
+      this.message="Age Should Be 18+"
+    }
+  }
+
 
 
   public close(status: string): void {
     this.opened = false;
+    this.openedDialog=false
   }
 
    openDialog(data:any,title:any) {
@@ -79,10 +99,16 @@ this.title=title
                 
     }
     console.log(obj)
-      
+
+  
   this.http.post(this.apiUrl,obj).subscribe(el=>{
     console.log(el)
+
     this.allData=el
+  if(this.allData.length === 0){
+    this.openedDialog=true;
+this.message="No Data Found"
+          }
       console.log(this.allData)   
       this.searchForm.reset();
   })
